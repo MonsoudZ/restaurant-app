e48c2b573471026c5be3724699286883e471d8a1
Monsoud Zanaty III <monsoud@Monsouds-Air.fios-router.home>
2022-06-14 00:20:43 -0400

    starting project for Don Juan Restaurant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {CartComponent} from "./cart/cart.component";
+import {FooterComponent} from "./footer/footer.component";
+import {LoginComponent} from "./login/login.component";
+import {MenuComponent} from "./menu/menu.component";
+import {NewUserComponent} from "./newUser/newUser.component";
+import {HomeComponent} from "./home/home.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    FooterComponent,
+    LoginComponent,
+    MenuComponent,
+    NewUserComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/newUser/newUser.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'app-newUser',
+  templateUrl: './newUser.component.html',
+  styleUrls: ['./newUser.component.css']
+})
+
+export class NewUserComponent {
+
+}
